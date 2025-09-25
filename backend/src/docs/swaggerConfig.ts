ec10104a6a1e5b6bbc76210ca9e3fc377b28d3a5
jonathanspereira <jonathanpereira.jsp@outlook.com>
2025-09-25 20:31:03 -0300

    Corrigindo Swagger para ser carregado no Docker

--- backend/src/docs/swaggerConfig.ts
+++ backend/src/docs/swaggerConfig.ts
@@ -3,9 +3,9 @@ import fs from 'fs';
 import path from 'path';
 import { Express } from 'express';
 
-
 export const setupSwagger = (app: Express) => {
-  const swaggerFile = path.join(process.cwd(), 'src', 'docs', 'swagger.json'); // <- caminho robusto
+  // Usa __dirname para apontar para dist/docs/swagger.json
+  const swaggerFile = path.join(__dirname, 'docs', 'swagger.json');
   const swaggerData = JSON.parse(fs.readFileSync(swaggerFile, 'utf8'));
 
   app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerData));
